2bb1b40c997517d79f9d15649e66889273a0a9de
jezekon <78500480+jezekon@users.noreply.github.com>
2025-04-04 13:31:42 +0200

    Update MeshGrid.jl

--- src/MeshGrid/MeshGrid.jl
+++ src/MeshGrid/MeshGrid.jl
@@ -1,25 +1,20 @@
 module MeshGrid
 
 
-export MeshInformations, elementToNodalValues, Mesh, TriangularMesh, PartOfModel, Grid, LinkedList, getMesh_AABB, generateGridPoints, extractSurfaceTriangularMesh, calculateMiniAABB_grid, ModiffElementalDensities, generateConnectivityArray, NodePosition3D, find_triangle_position, interactive_sdf_grid_setup, noninteractive_sdf_grid_setup, find_threshold_for_volume
+export MeshInformations, Mesh, TriangularMesh, Grid, LinkedList, getMesh_AABB, generateGridPoints, extractSurfaceTriangularMesh, calculateMiniAABB_grid, generateConnectivityArray, find_triangle_position, interactive_sdf_grid_setup, noninteractive_sdf_grid_setup
 
 using Statistics
 using LinearAlgebra
 using StaticArrays
-using FastGaussQuadrature
 using Printf
 using Base.Threads
-using Rho2sdf.ShapeFunctions
 using Rho2sdf.TerminalUtils
 
 include("MeshVolume.jl")
 include("MeshInformations.jl")
-include("PartOfModel.jl")
-include("NodalDensities.jl")
 include("SurfaceTriangularMesh.jl")
 include("Grid.jl")
 include("Grid_setup.jl")
-include("Isocontour_volume.jl")
 
 
 end
