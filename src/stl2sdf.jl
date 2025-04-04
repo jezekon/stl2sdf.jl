01d5c150322025f7a3217197031ee8ba4c5288f6
jezekon <78500480+jezekon@users.noreply.github.com>
2025-04-04 13:32:24 +0200

    Update stl2sdf.jl

--- src/stl2sdf.jl
+++ src/stl2sdf.jl
@@ -1,5 +1,30 @@
 module stl2sdf
 
-greet() = print("Hello World!")
+export MeshInformations
 
-end # module stl2sdf
+using LinearAlgebra
+using Statistics
+using DelimitedFiles
+using Einsum
+using BenchmarkTools
+
+# Tools for monitoring the computation process:
+include("TerminalUtils/TerminalUtils.jl")
+using .TerminalUtils
+
+include("MeshGrid/MeshGrid.jl")
+using .MeshGrid
+
+include("SignedDistances/SignedDistances.jl")
+using .SignedDistances
+
+# include("DataExport/DataExport.jl")
+# using .DataExport
+
+include("SdfSmoothing/SdfSmoothing.jl")
+using .SdfSmoothing
+
+# include("ImplicitDomainMeshing/ImplicitDomainMeshing.jl")
+# using .ImplicitDomainMeshing
+
+end # module Rho2sdf
