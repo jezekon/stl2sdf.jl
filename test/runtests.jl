618665444672c702a9fc0e972d0321333d8a07ea
jezekon <78500480+jezekon@users.noreply.github.com>
2025-04-04 15:28:00 +0200

    Create runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,15 @@
+using Test
+using LinearAlgebra
+using stl2sdf
+using stl2sdf.MeshGrid
+# using stl2sdf.SignedDistances
+# using stl2sdf.SdfSmoothing
+
+@testset "stl2sdf.jl" begin
+    # Test configuration flags
+    RUN_lin_beam = true
+
+    if RUN_lin_beam
+    
+    end
+end
