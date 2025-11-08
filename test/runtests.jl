af71e749ecab4877e94bf0a8dd853cf4add89ad3
jezekon <78500480+jezekon@users.noreply.github.com>
2025-11-08 19:32:14 +0100

    runtests.jl cleanup

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,16 +11,12 @@ using BenchmarkTools
 
 @testset "stl2sdf.jl" begin
     # Test configuration flags
-    Import_beam = false
-    Import_bunny = false
-    RUN_lin_beam = false
-    RUN_main_some_opts = false
+    Import_beam = true
     RUN_main_opts = true
-    RUN_beam_Mecas = false
+    RUN_beam = true
 
-    #NOTE:
     if Import_beam
-        taskName = "beam-approx"
+        taskName = "Beam"
 
         @time (X, IEN) = import_stl("../data/$(taskName).stl") # -> Vector of vectors
         println(typeof(X))
@@ -29,57 +25,6 @@ using BenchmarkTools
         println(typeof(IEN))
         println(size(IEN))
         println(IEN[1])
-    end
-
-    #NOTE:
-    if Import_bunny
-        # taskName = "StanfordBunny_small"
-        taskName = "StanfordBunny_large"
-
-        @time (X, IEN) = import_stl("../data/$(taskName).stl") # -> Vector of vectors
-    end
-
-    #NOTE:
-    if RUN_lin_beam
-        taskName = "beam-approx"
-        # taskName = "StanfordBunny_small"
-
-        N = 120  # Number of cells along the longest side
-
-        # Data from stl:
-        (X, IEN) = import_stl("../data/$(taskName).stl") # -> Vector of vectors
-        # (X, IEN) = import_stl("data/$(taskName).stl")
-
-        # Data from Tetgen
-        run_tetgen(taskName, "../data")  # Run in specified directory
-        (X_tet, IEN_tet) = import_tetgen_mesh("../data/$(taskName).1") # -> Vector of vectors
-        # (X_tet, IEN_tet) = import_tetgen_mesh("data/$(taskName).1")
-
-        TriMesh = TriangularMesh(X, IEN)
-        TetMesh = Mesh(X_tet, IEN_tet)
-
-        ## Grid:
-        X_min, X_max = getMesh_AABB(TriMesh.X)
-        sdf_grid = Grid(X_min, X_max, N, 3) # cartesian grid
-        points = generateGridPoints(sdf_grid) # uzly pravidelné mřížky
-
-        ## SFD from triangular mesh:
-        (dists, xp) = evalDistancesOnTriMesh(TriMesh, sdf_grid, points) # Vector{Float64}
-        (signs, _) = raycast_sign_detection(TriMesh, sdf_grid, points)
-        sdf_dists = dists .* signs
-
-        exportSdfToVTI(taskName * "_SDF.vti", sdf_grid, sdf_dists, "distance")
-
-        # RBF smoothing:
-        is_interp = false
-        (fine_sdf, fine_grid) =
-            RBFs_smoothing(TetMesh, sdf_dists, sdf_grid, is_interp, 2, taskName) # interpolation == true, aproximation == false, smooth
-        export_sdf_results(fine_sdf, fine_grid, sdf_grid, taskName, 2, is_interp)
-    end
-
-    if RUN_main_some_opts
-        options = SDFOptions(grid_step = 0.8)  # Use defaults for other parameters
-        result = stl_to_sdf("beam-approx.stl", options = options)
     end
 
     if RUN_main_opts
@@ -91,11 +36,11 @@ using BenchmarkTools
             remove_artifacts = true,
             artifact_ratio = 0.01,
         )
-        result = stl_to_sdf("../data/beam-approx.stl", options = options)
+        result = stl_to_sdf("../data/Beam.stl", options = options)
     end
 
-    if RUN_beam_Mecas
-        taskName = "beam_Mecas"
+    if RUN_beam
+        taskName = "Beam"
         N = 80  # Number of cells along the longest side
 
         # 1. Import STL file
